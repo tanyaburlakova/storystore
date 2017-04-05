cad8c8128e34f3bcff5f349c97c8c808f996d5d9
Таня Бурлакова <ta.burlakova@gmail.com>
2017-04-06 01:29:33 +0300

    no message

--- app/js/app.js
+++ app/js/app.js
@@ -28,4 +28,14 @@ $(() => {
     prevArrow: $('.js-slider-nav-prev'),
     nextArrow: $('.js-slider-nav-next')
   });
+
+  $('.js-product-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    dots: false,
+    focusOnSelect: true,
+    prevArrow: $('.js-product-slider-prev'),
+    nextArrow: $('.js-product-slider-next')
+  })
 });
